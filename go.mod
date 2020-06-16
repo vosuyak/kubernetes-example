ced04e3b4b0f0120c6657436bd181ff4b98cc0ef
vosuyak <vosyak@yahoo.com>
2020-06-16 19:43:12 -0400

    mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module kubemessage
+
+go 1.14
+
+require go.mongodb.org/mongo-driver v1.3.4 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"context"
+	"log"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func init() {
+	LoadEnv()
+}
 
 // GetMessage - get request to display message to the GUI
 func GetMessage(w http.ResponseWriter, r *http.Request) {
@@ -16,8 +29,37 @@ func routes() {
 	http.HandleFunc("/", GetMessage)
 }
 
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	initData()
+}
+
+func initData() {
+	connecturi := fmt.Sprintf(
+		"mongodb://%s:%s",
+		os.Getenv("MONGO_DB_HOST"),
+		os.Getenv("MONGO_DB_PORT"),
+	)
+	client, err := mongo.NewClient(options.Client().ApplyURI(connecturi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	quickstartDatabase := client.Database("quickstart")
+	fmt.Println(quickstartDatabase)
+}
+
 func main() {
-	fmt.Println("Connecting......")
+	fmt.Println("Connecting......", os.Getenv("MONGO_DB_HOST"))
 	routes()
 	http.ListenAndServe(":3000", nil)
 }
